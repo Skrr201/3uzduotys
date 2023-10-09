b2a9a8012aa0f837545e210dcf1221393c333044
Skrr201 <118713476+Skrr201@users.noreply.github.com>
2023-10-09 10:52:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Darbas 10-09</h1>
+
+    <?php 
+    //Pirma uzduotis
+    /* $numbers = [3.2, 6.4, 7.3, 9.2, 4.8];
+    $average = array_sum($numbers) / count($numbers);
+    echo $average;
+    */
+
+    //Antra uzduotis
+    /* $average = array('Jonas'=>6.2, 'Tadas'=>2.8, 'Saulius'=>5.3);
+
+    foreach ($average as $name => $value) {
+        if($value > 4) {$PositiveVidurkis = 'Teigiamas';}else{$PositiveVidurkis = 'Neigiamas';}
+        echo $name , ':' , $value , ' - ', $PositiveVidurkis, '<br>';
+    }
+    */
+
+    /*$mokiniai = array(
+        "Matas" => array("Matematika" => 7, "Istorija" => 5, "Chemija" => 5),
+        "Kazys" => array("Matematika" => 10, "Istorija" => 10, "Chemija" => 9),
+        "Gabija" => array("Matematika" => 4, "Istorija" => 6 , "Chemija" => 3)
+        );
+        
+        foreach ($mokiniai as $vardas => $dalykai) {
+        $vidurkis = array_sum($dalykai) / count($dalykai);
+        echo "Mokinys: $vardas, Vidurkis: $vidurkis ";
+        
+        if ($vidurkis > 4) {
+        echo "- gali pereiti į kitus metus.";
+        } else {
+        echo "- negali pereiti į kitus metus.";
+        }
+        
+        echo "<br>";
+        }
+        */
+
+    ?>
+</body>
+</html>
